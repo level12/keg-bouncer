59c91881a2354bf528bff5d45cf51ca5baca8937
Bill Adams <bill.adams@level12.io>
2016-12-12 18:54:16 -0500

    Drop python 3.4 support
    Ref GH-18

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # CHANGELOG
 
+## 2.1.0
+* Dropped Python 3.4 support
+
 ## 2.0.0
 * Redesign API
 * Add API for verifying passwords and keeping password history
--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
     packages=find_packages(exclude=['keg_bouncer_test_app*']),
